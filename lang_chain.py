532394105a19b41f8752f54141087b782b25eb6c
Daniel Santos <daniel.oliveira@abril.com.br>
2025-08-15 14:01:57 -0300

    start code

---
+++ lang_chain.py
@@ -0,0 +1,32 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage
+from my_models import GEMINI_FLASH
+from my_keys import GEMINI_API_KEY
+from my_helper import encode_image
+
+
+llm = ChatGoogleGenerativeAI(
+  api_key=GEMINI_API_KEY,
+  model=GEMINI_FLASH
+)
+
+imagem = encode_image("dados\exexemplo_grafico.jpg")
+
+pergunta = "descreva a imagem"
+
+mensagem = HumanMessage(
+    content = [
+        {
+            "type": "text",
+            "text": pergunta
+        },
+        {
+            "type": "image_url",
+            "image_url": f"data:image/jpeg;base64,{imagem}"
+        }
+    ]
+)
+
+resposta = llm.invoke([mensagem])
+
+print(resposta)
