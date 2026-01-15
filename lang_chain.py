7831bacaf15f9ff2c77c08d7a4c78486b40c9756
Daniel Santos <daniel.oliveira@abril.com.br>
2026-01-15 08:53:40 -0300

    modelo final analise de imagem maritaca

--- lang_chain.py
+++ lang_chain.py
@@ -1,44 +1,78 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.chat_models import ChatMaritalk
 from langchain_core.messages import HumanMessage
-from my_models import GEMINI_FLASH, MARITACA_SABIA
+from my_models import GEMINI_FLASH, GEMINI_PRO, MARITACA_SABIA
 from my_keys import GEMINI_API_KEY, MARITACA_API_KEY
 from my_helper import encode_image
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.globals import set_debug
 
+set_debug(True)
 
 llm = ChatGoogleGenerativeAI(
   api_key=GEMINI_API_KEY,
   model=GEMINI_FLASH
 )
 
-resposta = llm.invoke("Quais canais de Youtube você recomenda para que eu possa saber mais a respeito de smarpthones?")
-print("Gemini: ", resposta.content)
+# resposta = llm.invoke("Quais canais de Youtube você recomenda para que eu possa saber mais a respeito de smarpthones?")
+# print("Gemini: ", resposta.content)
 
 llm = ChatMaritalk(
   api_key=MARITACA_API_KEY,
   model=MARITACA_SABIA
 )
-resposta = llm.invoke("Quais canais de Youtube você recomenda para que eu possa saber mais a respeito de smarpthones?")
-
-print("Maritaca: ", resposta.content)
+# resposta = llm.invoke("Quais canais de Youtube você recomenda para que eu possa saber mais a respeito de smarpthones?")
+# print("Maritaca: ", resposta.content)
 
 imagem = encode_image("dados\exemplo_grafico.jpg")
 
-pergunta = "descreva a imagem"
+template_analisador = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Assuma que você é um analisador de imagens. Sua tarefa é analisar a imagem
+            e extrair informações de forma objetiva.
 
-mensagem = HumanMessage(
-    content = [
-        {
-            "type": "text",
-            "text": pergunta
-        },
-        {
-            "type": "image_url",
-            "image_url": f"data:image/jpeg;base64,{imagem}"
-        }
+            # FORMATO DE SAÍDA
+            Descrição da Imagem: 'Coloque a sua descrição da imagem aqui'
+            Rótulos: 'Coloque uma lista com três termos chave separados por vírgula'
+            """
+        ),
+        (
+            "user",
+            [
+                {"type" : "text", "text" : "Descreva a imagem:"},
+                {"type" : "image_url", "image_url" : {"url":"data:image/jpeg;base64,{imagem_informada}"}}
+            ]
+        )
     ]
 )
 
-resposta = llm.invoke([mensagem])
+cadeia_resumo = template_analisador | llm | StrOutputParser()
+# resposta = cadeia_resumo.invoke({"imagem_informada" : imagem})
+
+template_resposta = PromptTemplate(
+    template="""
+    Gere um resumo, utilizando uma linguagem clara e objetiva, focada no publico brasileiro.
+    A ideia é que a comunicação do resultado seja mais fácil possivel, priorizando registros para consultas posteriores.
+    
+    # Resultado da imagem
+    {resultado_imagem}
+    """,
+    input_variables=["resultado_imagem"]
+)
+
+llm_maritaca = ChatMaritalk(
+  api_key=MARITACA_API_KEY,
+  model=MARITACA_SABIA
+)
+
+cadeia_resumo = template_resposta | llm_maritaca | StrOutputParser()
+
+cadeia_competa = (template_analisador | cadeia_resumo)
+
+resposta = cadeia_competa.invoke({"imagem_informada" : imagem})
 
 print(resposta)
